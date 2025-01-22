12d4c9ad928039b3296545531899f296a2e5c337
Raajih Roland <raajihroland88@gmail.com>
2025-01-21 19:29:41 -0500

    Add project files.

---
+++ GameBag.cpp
@@ -0,0 +1,43 @@
+#include "GameBag.h"
+
+int GameBag::getCurrentSize() const
+{
+    return bag.size();
+}
+
+bool GameBag::isEmpty() const
+{
+    return (bag.size() == 0);
+}
+
+bool GameBag::add(const int& item)
+{
+    bag.push_back(item);
+
+}
+//Not done!!
+bool GameBag::remove(const int& item)
+{
+    
+}
+
+void GameBag::clear()
+{
+    bag.clear();
+}
+
+bool GameBag::contains(const int& item) const
+{
+    for (int bagItem : bag)
+    {
+        if (bagItem == item)
+            return true;
+    }
+    return false;
+}
+
+//Not finished!!
+std::vector<int> GameBag::toVector() const
+{
+    return std::vector<int>();
+}
